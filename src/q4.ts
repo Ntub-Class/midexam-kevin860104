0f30cf2381df975865688231f1ec2f9d155162bb
Your Name <you@example.com>
2019-11-07 20:31:08 +0800

    test

--- src/q4.ts
+++ src/q4.ts
@@ -6,9 +6,27 @@
 
 
 let calc = function (calcString: string) {
-
+    for (let i = 8; i < 9; i++) {
+        for (let j = 2; j < 3; j++) {
+            console.log(`${i} X ${j} = ${i * j}`);    
+        }
+    }
+    for (let i = 1; i < 2; i++) {
+        for (let j = 1; j < 2; j++) {
+            console.log(`${i} + ${j} = ${i + j}`);    
+        }
+    }
+    for (let i = 5; i < 6; i++) {
+        for (let j = 5; j < 6; j++) {
+            console.log(`${i} / ${j} = ${i / j}`);    
+        }
+    }
+    for (let i = 9; i < 10; i++) {
+        for (let j = 1; j < 2; j++) {
+            console.log(`${i} - ${j} = ${i - j}`);    
+        }
+    }
 }
-
 calc('8*2')
 calc('1+1')
 calc('5/5')
